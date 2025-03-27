f23c29102f724b957540e1c2811945f8de8fd05c
Dersim Abbas <dersim.abbas@iths.se>
2025-03-27 16:22:36 +0100

    testing if pipeline stops run twice

--- src/Backend/CloudNinjaAPI/Program.cs
+++ src/Backend/CloudNinjaAPI/Program.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,12 +9,13 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
+    options.AddPolicy(
+        "AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        }
+    );
 });
 
 // And in the Configure method:
@@ -30,23 +33,66 @@ app.UseHttpsRedirection();
 
 var init = new[]
 {
-    "Cloud ninja initalized.", "Create your pipelines", "write your infrastructure"
+    "Cloud ninja initalized.",
+    "Create your pipelines",
+    "write your infrastructure",
 };
 
 var summaries = new[]
 {
-    "Cloud ninja initalized.", "Create your docker compose ", "we are at the end of this infrastructure", "help me", "dead inside", "help me D:", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Cloud ninja initalized.",
+    "Create your docker compose ",
+    "we are at the end of this infrastructure",
+    "help me",
+    "dead inside",
+    "help me D:",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching",
 };
 
-app.MapGet("api/init", () =>
-{
-    var load = summaries[new Random().Next(summaries.Length)];
-    if (load == null)
+app.MapGet(
+    "api/init",
+    () =>
     {
-        return Results.NotFound();
+        var load = summaries[new Random().Next(summaries.Length)];
+        if (load == null)
+        {
+            return Results.NotFound();
+        }
+        return Results.Ok(load);
     }
-    return Results.Ok(load);
-});
+);
+
+app.MapGet(
+    "/api/FortressBreach",
+    async () =>
+    {
+        var httpClient = new HttpClient();
+        var functions_url =
+            $"https://containerfunctionstest-bsasfvawb5gfdqh7.swedencentral-01.azurewebsites.net/api/fortressbreach?resourcegroupname=rg-dersimabbas";
+
+        var response = httpClient.GetAsync(functions_url);
+        var jsonResult = await response.Result.Content.ReadAsStringAsync();
+
+        return Results.Content(jsonResult, "application/json");
+    }
+);
+
+app.MapGet(
+    "/api/NinjaWebScan",
+    async () =>
+    {
+        var httpClient = new HttpClient();
+        var functions_url =
+            $"https://containerfunctionstest-bsasfvawb5gfdqh7.swedencentral-01.azurewebsites.net/api/WebAppNinjaScan?resourcegroupname=rg-dersimabbas";
+
+        var response = httpClient.GetAsync(functions_url);
+        var jsonResult = await response.Result.Content.ReadAsStringAsync();
+        //test
+        return Results.Content(jsonResult, "application/json");
+    }
+);
 
 app.Run();
-
